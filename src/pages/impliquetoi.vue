0212f1a2ae889703e5be6e17d7dd4924bc8b7304
jaubut <jeremie.aubut@gmail.com>
2018-01-22 10:40:09 -0500

    correction des fautes

--- src/pages/impliquetoi.vue
+++ src/pages/impliquetoi.vue
@@ -4,14 +4,14 @@
     <div class="bloc span-2">
       <div class="texte">
         <h2>Fais partie des coulisses de Sainte Scène</h2>
-        <p>C'est super simple. Ici à la <strong>Sainte Scène</strong> on aime travailler en équipe et on croit que de faire partie de la communauté passe par notre implication. On a des départements qui joue de la musique, à gérer la console de la régie, à faire partie de l'équipe multi-média, gérer un groupe de la relève, etc.</p>
+        <p>C'est super simple. Ici à la <strong>Sainte Scène</strong> on aime travailler en équipe et on croit que de faire partie de la communauté passe par notre implication. On a des départements qui jouent de la musique, à gérer la console de la régie, à faire partie de l'équipe multi-média, gérer un groupe de la relève, etc.</p>
       </div>
     </div>
     <div class="bloc span-2 grey row">
       <div class="texte">
         <img src="../assets/Slack_Mark.svg" alt="">
         <h3>On aime Slack</h3>
-        <p>Slack est une application formidable qui représente un hub centrale pour toute l'équipe de <strong>Sainte Scène</strong></p>
+        <p>Slack est une application formidable qui représente un hub central pour toute l'équipe de <strong>Sainte Scène</strong></p>
       </div>
       <img src="../assets/slack/photo-1.png" alt="">
     </div>
